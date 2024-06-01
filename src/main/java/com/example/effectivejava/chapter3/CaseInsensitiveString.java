1e3415604a55ac7113550b21acdef90847ce0296
seojeonghyeon <seojeonghyeon0630@gmail.com>
2024-06-01 10:45:50 +0900

    [Chapter3] equals 규약(반사성, 대칭성)

---
+++ src/main/java/com/example/effectivejava/chapter3/CaseInsensitiveString.java
@@ -0,0 +1,45 @@
+package com.example.effectivejava.chapter3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public final class CaseInsensitiveString {
+    private final String s;
+
+    public CaseInsensitiveString(String s) {
+        this.s = Objects.requireNonNull(s);
+    }
+
+    //대칭성 위배
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof CaseInsensitiveString) {
+            return s.equalsIgnoreCase(((CaseInsensitiveString) obj).s);
+        }
+        if (obj instanceof String) {
+            return s.equalsIgnoreCase((String) obj);
+        }
+        return false;
+    }
+
+    //수정된 equals 메서드(56페이지)
+//    @Override
+//    public boolean equals(Object obj) {
+//        return (obj instanceof CaseInsensitiveString) &&
+//                (((CaseInsensitiveString) obj).s.equalsIgnoreCase(s));
+//    }
+
+
+    public static void main(String[] args) {
+        CaseInsensitiveString cis = new CaseInsensitiveString("Polish");
+        String polish = "polish";
+        System.out.println(cis.equals(polish));
+
+        List<CaseInsensitiveString> list = new ArrayList<>();
+        list.add(cis);
+        System.out.println(list.contains(polish));
+    }
+
+
+}
